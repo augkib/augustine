1b83a994e9167f8347d601b89d7ff843457efdf4
augkib <serbei2004@gmail.com>
2025-03-28 10:36:49 +0300

    Add files via upload

---
+++ augustine_python_asignment_week_1.py
@@ -0,0 +1,10 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> x=int(input('Input the value of x: '))
+Input the value of x: 10
+>>> y=int(input('Input the value of y: '))
+Input the value of y: 20
+>>> add=x+y
+>>> print('The sum of x and y is ',add)
+The sum of x and y is  30
